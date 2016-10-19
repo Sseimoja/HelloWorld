7e337345359d6b876e90a701f55c65b8d2badba3
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-19 13:56:58 +0300

    Initial commit

---
+++ src/projekt.java
@@ -0,0 +1,9 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class projekt {public static void main(String[] args) {
+    System.out.print("Hello, ");
+    System.out.print("World!");
+
+}}
+
